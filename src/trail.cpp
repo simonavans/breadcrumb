28a998808002d0a2c648e176a054047c1ab0910b
simonavans <saj.decock@student.avans.nl>
2025-03-29 22:29:18 +0100

    fix: trail module setting timestamp

--- src/trail.cpp
+++ src/trail.cpp
@@ -6,12 +6,12 @@
 #define TRAIL_STORE_SIZE  512
 #define TRAIL_MAX_DENSITY 16
 
-// LIFO queue?
 static location loc_store[TRAIL_STORE_SIZE];
 static size_t loc_store_size = 0;
 
 void trail_store_loc(location* loc) {
     location new_loc;
+    new_loc.timestamp           = loc->timestamp;
     new_loc.lat                 = loc->lat;
     new_loc.lng                 = loc->lng;
     loc_store[loc_store_size++] = new_loc;
@@ -40,6 +40,7 @@ void trail_get_past_loc(location* loc) {
             int store_idx =
                 ((float)idx / (float)TRAIL_MAX_DENSITY) * loc_store_size - 1;
             location stored_loc = loc_store[store_idx];
+            loc->timestamp      = stored_loc.timestamp;
             loc->lat            = stored_loc.lat;
             loc->lng            = stored_loc.lng;
             return;
